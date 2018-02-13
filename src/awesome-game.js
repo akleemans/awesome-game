b9100678ec503f3039268044e1a02f95a3e88232
Adrianus Kleemans <a.kleemans@gmail.com>
2018-02-13 17:21:20 +0100

    Object layer, pit of death, tweaking physics

--- src/awesome-game.js
+++ src/awesome-game.js
@@ -7,28 +7,36 @@ var game = new Phaser.Game(352, 192, Phaser.CANVAS, 'phaser-example', {
 });
 
 function preload() {
-
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
   game.load.image('background', 'assets/background2.png');
 
-  game.load.tilemap('base-level', 'assets/tilemaps/maps/test16.json', null, Phaser.Tilemap.TILED_JSON);
+  game.load.tilemap('base-level', 'assets/tilemaps/maps/level0.json', null, Phaser.Tilemap.TILED_JSON);
 
   //  Next we load the tileset. This is just an image, loaded in via the normal way we load images:
   game.load.image('tiles', 'assets/tilemaps/tiles/platformer_tiles.png');
 }
 
 var player;
-var facing = 'left';
+var facing = 'right';
 var jumpTimer = 0;
 var cursors;
 var jumpButton;
 var backgroundLayer;
 var baseLayer;
 var collisionLayer;
-var baseSpeed = 250;
+var baseSpeed = 200;
 
 var map;
 var layer;
+
+// map meta objects
+var finishRectangle;
+var startRectangle;
+var torchRectangle;
+var pitRectangle;
+
+var statusText;
+var statusTimer;
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -52,13 +60,15 @@ function create() {
   //  This resizes the game world to match the layer dimensions
   collisionLayer.resizeWorld();
 
+  prepareMetaObjects();
+
   // player
-  player = game.add.sprite(32, 20, 'dude');
+  player = game.add.sprite(32, 100, 'dude');
   game.physics.enable(player, Phaser.Physics.ARCADE);
 
   player.body.collideWorldBounds = true;
   player.body.gravity.y = 1000;
-  player.body.maxVelocity.y = 800;
+  player.body.maxVelocity.y = 500;
   player.body.setSize(20, 32, 5, 16);
 
   player.animations.add('left', [0, 1, 2, 3], 10, true);
@@ -73,17 +83,15 @@ function create() {
   game.scale.setUserScale(2, 2);
 
   // enable crisp rendering
-  game.renderer.renderSession.roundPixels = true;
-  Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
+  // game.renderer.renderSession.roundPixels = true;
+  Phaser.Canvas.setImageRenderingCrisp(game.canvas);
 
   // add input
   cursors = game.input.keyboard.createCursorKeys();
   jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
 }
 
 function update() {
-  this.game.physics.arcade.collide(player, collisionLayer);
 
   player.body.velocity.x = 0;
 
@@ -118,9 +126,35 @@ function update() {
     }
   }
 
+  this.game.physics.arcade.collide(player, collisionLayer);
+
+
+  if (Phaser.Rectangle.containsPoint(finishRectangle, player.position)) {
+    console.log("reached finish");
+  }
+
+  if (Phaser.Rectangle.containsPoint(pitRectangle, player.position)) {
+    console.log("reached pit");
+    // TODO
+    resetPlayer();
+  }
+
+  if (Phaser.Rectangle.containsPoint(torchRectangle, player.position)) {
+    console.log("reached torch");
+    // TODO
+  }
+
+
+  /*
+  // idea for later: slide down walls :-)
+  this.game.physics.arcade.collide(player, collisionLayer, function(p) {
+    p.body.velocity.y = 0;
+  });
+   */
+
   if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
-    player.body.velocity.y = -500;
-    jumpTimer = game.time.now + 750;
+    player.body.velocity.y = -350;
+    jumpTimer = game.time.now + 300;
   }
 
 }
@@ -129,6 +163,49 @@ function render() {
 
   // debug info
   // game.debug.bodyInfo(player, 16, 24);
-  // game.debug.body(player);
 
+  /*
+  game.debug.body(player);
+  game.debug.geom(finishRectangle, 'rgba(255,0,0,0.2)');
+  game.debug.geom(pitRectangle, 'rgba(255,0,0,0.2)');
+  game.debug.geom(torchRectangle, 'rgba(255,0,0,0.2)');
+  */
+
+  // game.debug.text( "This is debug text", 100, 380 );
+
+}
+
+function prepareMetaObjects() {
+  // prepare start rectangle
+  var start = map.objects.meta.find(function (o) {
+    return o.name === 'start'
+  });
+  startRectangle = new Phaser.Rectangle(start.x, start.y, start.width, start.height);
+  console.log("startRectangle:", startRectangle);
+
+  // prepare finish rectangle
+  var finish = map.objects.meta.find(function (o) {
+    return o.name === 'finish'
+  });
+  finishRectangle = new Phaser.Rectangle(finish.x, finish.y, finish.width, finish.height);
+  console.log("finishRectangle:", finishRectangle);
+
+  // prepare finish rectangle
+  var pit = map.objects.meta.find(function (o) {
+    return o.name === 'pit'
+  });
+  pitRectangle = new Phaser.Rectangle(pit.x - 1, pit.y - 1, pit.width + 2, pit.height + 2);
+  console.log("pitRectangle:", pitRectangle);
+
+  // prepare torch rectangle
+  var torch = map.objects.meta.find(function (o) {
+    return o.name === 'torch'
+  });
+  torchRectangle = new Phaser.Rectangle(torch.x - 1, torch.y - 1, torch.width + 2, torch.height + 2);
+  console.log("torchRectangle:", torchRectangle);
+}
+
+function resetPlayer() {
+  player.position.set(startRectangle.x, startRectangle.y);
+  player.angle = 0;
 }
